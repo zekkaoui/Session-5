26b137a43eeb3261ff621f47ec10c03e120c7f35
zekkaoui <zekkaoui@gmail.com>
2019-01-23 14:16:16 +0000

    Add files via upload

---
+++ src/main/java/ai/alphalupitech/exemple/exemple/ExempleApplication.java
@@ -0,0 +1,14 @@
+package ai.alphalupitech.exemple.exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExempleApplication.class, args);
+	}
+
+}
+
